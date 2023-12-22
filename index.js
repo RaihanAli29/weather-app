f59561c2062857d18bf6e0c34c3ee2b84c84122f
RaihanAli29 <149912064+RaihanAli29@users.noreply.github.com>
2023-12-22 18:03:20 +0000

    Simulate a click on the search button when Enter key is pressed

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const container = document.querySelector('.container');
 const search = document.querySelector('.search-box button');
 const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
-const cityInput = document.querySelector('.search-box input'); // Added a variable for the city input
+const cityInput = document.querySelector('.search-box input'); // Added missing variable declaration for cityInput
 
-// Function to handle the click event
 search.addEventListener('click', () => {
     const APIKey = '3f6cdcb424f3ec3e3c166dcb77ec4675';
-    const city = cityInput.value; // Changed ariaValueMax to value
+    const city = cityInput.value; // Changed document.querySelector('.search-box input') to cityInput
 
-    if (city === '') {
+    if (city === '')
         return;
-    }
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
         .then(response => response.json())
@@ -19,13 +18,14 @@ search.addEventListener('click', () => {
             if (json.cod === '404') {
                 container.style.height = '400px';
                 weatherBox.style.display = 'none';
+                weatherDetails.style.display = 'none';
                 error404.style.display = 'block';
                 error404.classList.add('fadeIn');
                 return;
             }
 
             error404.style.display = 'none';
-            error404.classList.add('fadeIn');
+            error404.classList.remove('fadeIn');
 
             const image = document.querySelector('.weather-box img');
             const temperature = document.querySelector('.weather-box .temperature');
@@ -59,12 +59,13 @@ search.addEventListener('click', () => {
             wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
 
             weatherBox.style.display = '';
+            weatherDetails.style.display = '';
             weatherBox.classList.add('fadeIn');
+            weatherDetails.classList.add('fadeIn');
             container.style.height = '590px';
         });
 });
 
-// Function to handle the enter key press
 cityInput.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
         search.click(); // Simulate a click on the search button when Enter key is pressed
